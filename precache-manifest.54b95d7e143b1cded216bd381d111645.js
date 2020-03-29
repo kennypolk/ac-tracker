8c7b327645cd5f5d6b2ffc5d9e6b9e477f24e14c
Ken Polk <kenny.polk1@gmail.com>
2020-03-29 05:44:59 -0500

    Updates

---
+++ precache-manifest.54b95d7e143b1cded216bd381d111645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98155ef8994434a2c0234cd55b121e86",
+    "url": "/actracker/index.html"
+  },
+  {
+    "revision": "91be5c792208843ee272",
+    "url": "/actracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ea19f7df485c292aa92",
+    "url": "/actracker/static/js/2.7bdb17e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/actracker/static/js/2.7bdb17e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91be5c792208843ee272",
+    "url": "/actracker/static/js/main.4c13d834.chunk.js"
+  },
+  {
+    "revision": "794afb3482af7082a1f1",
+    "url": "/actracker/static/js/runtime-main.9ff5f02f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/actracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
